1d9aafb97f9eead372e9ece4da8eec42d0a6a862
graftss <nfireman1@gmail.com>
2024-04-27 21:03:47 -0400

    add twitch stream

--- src/twitch/createTwitchBot.ts
+++ src/twitch/createTwitchBot.ts
@@ -36,6 +36,7 @@ export const createTwitchBot = (): Promise<[string, number]> => {
       '#boulder1145',
       '#dunewacky',
       '#enzor_au',
+      '#freeside11',
       '#game1t',
       '#holdenpnw',
       '#hotdogturtle',
